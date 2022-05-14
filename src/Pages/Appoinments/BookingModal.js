6af5ef8c53ac601c1083428a273043b1eaff2277
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-05-14 11:35:12 +0600

    loaded data from database

--- src/Pages/Appoinments/BookingModal.js
+++ src/Pages/Appoinments/BookingModal.js
@@ -1,6 +1,16 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment }) => {
+const BookingModal = ({ treatment, date , setTreatment}) => {
+    const {_id, name,  slots } = treatment
+
+const handleBooking =(e)=>{
+    e.preventDefault ();
+    const slot = e.target.slot.value;
+    console.log(_id, slot, name, date)
+    setTreatment(null);
+}
+
     return (
         <div>
 
@@ -8,11 +18,25 @@ const BookingModal = ({ treatment }) => {
             <div class="modal modal-bottom sm:modal-middle">
                 <div class="modal-box">
                     <label for="my-modal-6" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 class="font-bold text-lg text-primary">{treatment.name}</h3>
-                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <div class="modal-action">
+                    <h3 class="font-bold text-lg text-primary text-center mb-8">Booking for: {treatment.name}</h3>
+                    <form onSubmit={handleBooking}  className='grid grid-cols-1 gap-5 justify-items-center'>
+                        <input type="text" disabled value={format(date, 'PP')} class="input input-bordered w-full max-w-xs" />
+                        <select name='slot' class="select select-bordered w-full max-w-xs">
+                            {
+                                slots.map(slot => (<option value={slot} >{slot}
+
+                                </option>))
+                            }
+
+                        </select>
+                        <input type="text" name='name' placeholder="Full Name" class="input input-bordered w-full max-w-xs" />
+                        <input type='number' phone='phone' placeholder="Phone Number" class="input input-bordered w-full max-w-xs" />
+                        <input type="email" email='email' placeholder="Email" class="input input-bordered w-full max-w-xs" />
+                        <input type="submit" value="Submit" class="btn btn-primary w-full max-w-xs" />
+                    </form>
+                    {/* <div class="modal-action">
                         <label for="my-modal-6" class="btn">OK</label>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div >
--- src/Pages/Appoinments/Service.js
+++ src/Pages/Appoinments/Service.js
@@ -1,5 +1,3 @@
-import React, { useEffect, useState } from 'react';
-import ButtonLink from '../Home/ButtonLink';
 
 const Service = ({ service, setTreatment}) => {
     const { name, slots } = service;
@@ -29,7 +27,9 @@ const Service = ({ service, setTreatment}) => {
                     <p className='text-accent text-lg'>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} availabe</p>
                     <div class="card-actions justify-center">
                     {/* <button >Book Appoinment</button> */}
-                    <label for="my-modal-6" onClick={()=> setTreatment(service)} class=" modal-button btn bg-gradient-to-l from-primary to-secondary uppercase text-white border-none text-sm">Book Appoinment</label>
+                    <label  for="my-modal-6" 
+                    disabled ={slots.length===0}
+                    onClick={()=> setTreatment(service)} class=" modal-button btn bg-gradient-to-l from-primary to-secondary uppercase text-white border-none text-sm" >Book Appoinment</label>
 
                     </div>
                 </div>
